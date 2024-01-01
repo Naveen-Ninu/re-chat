84a2bd6ceedb4959fa2ddeb5bf8ed8bef186c680
mk0029 <mkhicher404@gmail.com>
2024-01-01 12:50:06 +0530

    update css/t

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LogIn from "./components/login/LogIn";
 
 function App() {
   return (
     <div className="App">
-   <h1 className='text-center mt-20 text-red-600 text-2xl'>start </h1>
+      <h1 className="text-center mt-20 text-red-600 text-2xl">start </h1>
+      <LogIn />
     </div>
   );
 }
